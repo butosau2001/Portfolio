0831691a1a1f6f2232f2c9a397e18b4b9601d5ff
Bruno <brunoborges2001@gmail.com>
2019-12-09 10:18:09 -0300

    sw

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -134,22 +134,20 @@ export function unregister() {
   }
 }
 
-// export const registerServiceWorker = () => {
-//   if ("serviceWorker" in navigator) {
-//     window.addEventListener("load", function() {
-//       navigator.serviceWorker
-//         .register("/sw.js")
-//         .then(function(registration) {
-//           // Registration was successful
-//           console.log(
-//             "ServiceWorker registration successful with scope: ",
-//             registration.scope
-//           );
-//         })
-//         .catch(function(err) {
-//           // registration failed :(
-//           console.log("ServiceWorker registration failed: ", err);
-//         });
-//     });
-//   }
-// };
+export const registerServiceWorker = () => {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      if (!isLocalhost)
+        navigator.serviceWorker
+          .register("/sw.js")
+          .then(function(registration) {
+            // Registration was successful
+            //console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          })
+          .catch(function(err) {
+            // registration failed :(
+            //console.log('ServiceWorker registration failed: ', err);
+          });
+    });
+  }
+};
